2e3e9d2749db2c9e112fa69a57818e754de091b5
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2018-11-02 15:27:55 +0900

    draft implementation

--- types/hubot/index.d.ts
+++ types/hubot/index.d.ts
@@ -6,8 +6,18 @@
 
 declare namespace Hubot {
     class Brain {
-        userForId(id: any): any;
-        userForName(name: string): any;
+        set(key: any, value: any): any;
+        get(key: any): any;
+        save(): void;
+        close(): void;
+        setAutoSave(enabled: boolean): void;
+        resetSaveInterval(seconds: number): void;
+        mergeData(data: any): void;
+        userForId(id: any): User;
+        userForId(id: any, options: any): User;
+        userForName(name: string): User;
+        usersForRawFuzzyName(fuzzyName: string): User[];
+        usersForFuzzyName(fuzzyName: string): User[];
     }
 
     class User {
@@ -17,17 +27,38 @@ declare namespace Hubot {
 
     class Message {
         user: User;
+        done: boolean;
+        room: Room;
+    }
+
+    class TextMessage extends Message {
         text: string;
-        id: string;
+        id: any;
+
+        match(regex: RegExp): RegExpMatchArray;
+        toString(): string;
+    }
+
+    class EntryMessage extends Message{}
+    class LeaveMessage extends Message{}
+    class TopicMessage extends TextMessage{}
+    class CatchAllMessage extends Message {
+      message: Message;
     }
 
     class Response<R> {
-        match: RegExpMatchArray;
+        robot: Robot;
         message: Message;
+        match: RegExpMatchArray;
+        envelope: { room: Room, user: User, message: Message };
+
 
         constructor(robot: R, message: Message, match: RegExpMatchArray);
         send(...strings: string[]): void;
+        emote(...strings: string[]): void;
         reply(...strings: string[]): void;
+        topic(...strings: string[]): void;
+        play(...strings: string[]): void;
         random<T>(items: T[]): T;
     }
 
